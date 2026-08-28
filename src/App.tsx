66d574412ff0991babd4c34cb469b0471afd661e
Razan <razan_az@outlook.com>
2026-08-28 17:41:27 +0300

    Update deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 //  * SPDX-License-Identifier: Apache-2.0
 //  */
 
-import NotFound from "./components/NotFound";
+// import NotFound from "./components/NotFound";
 
 // import React, { useState, useEffect, useRef } from 'react';
 // import { motion, AnimatePresence, useReducedMotion } from 'motion/react';
